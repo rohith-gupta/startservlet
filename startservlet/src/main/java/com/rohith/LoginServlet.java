3a565843aa0d3391f77a106d2912a926bd68c30e
rohith-gupta <rmarri@my.bridgeport.edu>
2025-01-22 17:44:02 -0500

    my second commit

--- startservlet/src/main/java/com/rohith/LoginServlet.java
+++ startservlet/src/main/java/com/rohith/LoginServlet.java
@@ -5,12 +5,13 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	
 	
